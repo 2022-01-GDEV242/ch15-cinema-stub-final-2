867d4c0f1b0b2c582f26d3be1f21e4147574da6c
Max Micheller <90006166+MaxMicheller@users.noreply.github.com>
2022-04-25 11:57:15 -0400

    fixed spaces

--- GitHubDemoClass.java
+++ GitHubDemoClass.java
@@ -20,12 +20,10 @@ public class GitHubDemoClass
         x = 0;
     }
 
-      public void printCrosbie(){
+    public void printCrosbie(){
         System.out.println("My name is Bill Crosbie");
-        
     }
     
-
     public void printJeremiah(){
         System.out.println("My name is Jeremiah Curtis");
     }
