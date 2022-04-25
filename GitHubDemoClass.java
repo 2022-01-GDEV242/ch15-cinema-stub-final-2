552964b5a9fbe3ac489ab62d1bc85448338c09d7
JEREMIAH CURTIS <89932629+JeremiahC0510@users.noreply.github.com>
2022-04-25 11:51:43 -0400

    Collapse-gitcollab tests
    Added print Jeremiah
    
    Added, printMax method
    
    Cleaned up method collision
    
    fixed spaces
    
    Created printEddie method
    
    -added javadocs to printEddie method
    
    created max class
    
    Added Jeremiah.java
    
    Created MyName Class
    
    ...

--- GitHubDemoClass.java
+++ GitHubDemoClass.java
@@ -20,11 +20,26 @@ public class GitHubDemoClass
         x = 0;
     }
 
-      public void printCrosbie(){
+    public void printCrosbie(){
         System.out.println("My name is Bill Crosbie");
-        
     }
     
+    public void printJeremiah(){
+        System.out.println("My name is Jeremiah Curtis");
+    }
+
+    public void printMax(){
+        System.out.println("My name is Max Micheller");
+    }
     
-    
+    /**
+     * This method prints the name of the one known as Eddie.
+     */
+    public void printEddie(){
+        System.out.println("My name is Eddie G!");
+    }
+
 }
+    
+    
+    
